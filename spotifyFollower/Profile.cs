1deed0edfb560803af48afb01a95bba09fe13fca
Roalexx <ahmetdmrlceng@gmail.com>
2024-06-17 00:43:11 +0300

    Add project files.

---
+++ spotifyFollower/Profile.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+
+public class Profile
+{
+    [JsonProperty("uri")]
+    public string Uri { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("image_url")]
+    public string ImageUrl { get; set; }
+
+    [JsonProperty("followers_count")]
+    public int FollowersCount { get; set; }
+
+    [JsonProperty("is_followed")]
+    public bool IsFollowed { get; set; }
+
+    [JsonProperty("is_following")]
+    public bool IsFollowing { get; set; }
+
+    [JsonProperty("color")]
+    public int Color { get; set; }
+}
+
+public class ExternalUrls
+{
+    [JsonProperty("spotify")]
+    public string Spotify { get; set; }
+}
+
+public class Image
+{
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("height")]
+    public int Height { get; set; }
+
+    [JsonProperty("width")]
+    public int Width { get; set; }
+}
+
+public class Followers
+{
+    [JsonProperty("href")]
+    public object Href { get; set; }
+
+    [JsonProperty("total")]
+    public int Total { get; set; }
+}
+
+public class ExplicitContent
+{
+    [JsonProperty("filter_enabled")]
+    public bool FilterEnabled { get; set; }
+
+    [JsonProperty("filter_locked")]
+    public bool FilterLocked { get; set; }
+}
+
+public class User
+{
+    [JsonProperty("display_name")]
+    public string DisplayName { get; set; }
+
+    [JsonProperty("external_urls")]
+    public ExternalUrls ExternalUrls { get; set; }
+
+    [JsonProperty("href")]
+    public string Href { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("images")]
+    public List<Image> Images { get; set; }
+
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("uri")]
+    public string Uri { get; set; }
+
+    [JsonProperty("followers")]
+    public Followers Followers { get; set; }
+
+    [JsonProperty("country")]
+    public string Country { get; set; }
+
+    [JsonProperty("product")]
+    public string Product { get; set; }
+
+    [JsonProperty("explicit_content")]
+    public ExplicitContent ExplicitContent { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+}
+
+public class Root
+{
+    [JsonProperty("profiles")]
+    public List<Profile> Profiles { get; set; }
+}
---
+++ spotifyFollower/Program.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main()
+    {
+        await Console.Out.WriteLineAsync("Spotify follower accounting v0.1");
+
+        await Console.Out.WriteLineAsync("Warning this app is on Early Access follow https://github.com/Roalexx for new versions");
+
+        await Console.Out.WriteLineAsync("first go to your profile in website then press ctrl+shift+c then go to network tab and press followers then right click onfollowers?market=from_token press copy and copy as curl(bash) and paste this curl here");
+
+        string curlCommand = ReadMultilineInput(); //we took curl
+
+        string command = curlCommand.ToString();
+
+        string authorization = authorizationFinder(command); //we took auth we need auth for all requests 
+
+        JObject myInfo = await HttpRequest_me(authorization); //we need user id so we took our data
+
+        string myId = (string)myInfo["id"]; // and weeding our id for next request
+
+        JObject followers = await HttpRequest_followers(authorization,myId);
+
+        JObject following = await HttpRequest_following(authorization,myId);
+
+        await Console.Out.WriteLineAsync(following.ToString());
+
+    }
+
+    static string ReadMultilineInput()
+    {
+        string input;
+        string result = string.Empty;
+
+        while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+        {
+            result += input + " ";
+        }
+        return result;
+    }
+
+    static string authorizationFinder(string command)
+    {
+        int startIndex = command.IndexOf("authorization:");
+
+        if (startIndex == -1)
+        {
+            return null;
+        }
+
+        startIndex += "authorization".Length;
+
+        string endSymbol = "\"";
+        int endIndex = command.IndexOf(endSymbol, startIndex);
+
+        if (endIndex == -1)
+        {
+            return null;
+        }
+
+        string cookieValue = command.Substring(startIndex + 2, endIndex - startIndex - 2).Trim();
+
+        return cookieValue;
+    }
+
+    static async Task<JObject> HttpRequest_me(string authorization)
+    {
+        string url = "https://api.spotify.com/v1/me";
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("authorization", authorization);
+            HttpResponseMessage response = await client.GetAsync(url);
+            JObject jsonObject;
+            string content = await response.Content.ReadAsStringAsync();
+            jsonObject = JObject.Parse(content);
+            return jsonObject;
+        }
+    }
+
+    static async Task<JObject> HttpRequest_followers(string authorization, string id)
+    {
+        string url = "https://spclient.wg.spotify.com/user-profile-view/v3/profile/" + id + "/followers?market=from_token";
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("authorization", authorization);
+            HttpResponseMessage response = await client.GetAsync(url);
+            JObject jsonObject;
+            string content = await response.Content.ReadAsStringAsync();
+            jsonObject = JObject.Parse(content);
+            return jsonObject;
+        }
+    }
+
+    static async Task<JObject> HttpRequest_following(string authorization, string id)
+    {
+        string url = "https://spclient.wg.spotify.com/user-profile-view/v3/profile/" + id + "/following?market=from_token";
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("authorization", authorization);
+            HttpResponseMessage response = await client.GetAsync(url);
+            JObject jsonObject;
+            string content = await response.Content.ReadAsStringAsync();
+            jsonObject = JObject.Parse(content);
+            return jsonObject;
+        }
+    }
+
+}
